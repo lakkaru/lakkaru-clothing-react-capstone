8498609c7db953e1204d9e8af3176869495a341b
Lakshman Karunasena <lakkaru@gmail.com>
2024-02-02 13:17:54 +0530

    fix-adding Mens category

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
 const App = () => {
-  return <div className="categories-container">
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-
-    </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-
-    </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-
-    </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-
-    </div>
-    <div className="category-container">
-      {/* <img/> */}
-      <div className="category-body-container">
-        <h2>Womens</h2>
-        <p>Shop Now</p>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default App;
